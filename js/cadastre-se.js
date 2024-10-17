15d61d98018da1c988d109fa8c1926a6c76049b6
Vagner Beraldo <vagnerberaldo1978@gmail.com>
2024-10-17 18:10:35 -0300

    atualização do main

--- js/cadastre-se.js
+++ js/cadastre-se.js
@@ -2,9 +2,15 @@
 document.getElementById('id-cadastrese').addEventListener('click', function() {
      // Seleciona os elementos para ocultar
      document.querySelector('.section-boasVindas').style.display = 'none';
+     document.querySelector('.sobre').style.display = 'none';
+     document.querySelector('.section-vagas').style.display = 'none';
      document.querySelector('.section-1').style.display = 'none';
      document.querySelector('.section-2').style.display = 'none';
-     document.querySelector('.depoimentos').style.display = 'none';
+     document.querySelector('.section-3').style.display = 'none';
+     document.getElementById('depCliente').style.display = 'none';
+     document.querySelector('.depoimento').style.display = 'none';
+
+
 
      // Exibe o iframe de cadastro
      document.getElementById('id-formCadastro').style.display = 'block';
